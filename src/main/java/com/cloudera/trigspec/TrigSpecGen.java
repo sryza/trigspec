a411ddbd756121b72f5fd9d4b134aea59b48a775
Sandy Ryza <sandy@cloudera.com>
2013-05-01 10:04:34 -0700

    Correct gen and print stuff in mapper

--- src/main/java/com/cloudera/trigspec/TrigSpecGen.java
+++ src/main/java/com/cloudera/trigspec/TrigSpecGen.java
@@ -18,7 +18,7 @@ public class TrigSpecGen {
     Path inputDir = new Path(args[0]);
     int numInputs = Integer.parseInt(args[1]);
     // means average task time should be (3 + 2 * 4) / 5 = 2.2 minutes
-    int numStragglers = Math.min(1, numInputs / 5);
+    int numStragglers = Math.max(1, numInputs / 5);
     Configuration conf = new Configuration();
     
     for (int i = 0; i < numInputs; i++) {
@@ -26,8 +26,8 @@ public class TrigSpecGen {
       Path path = new Path(inputDir, "input_" + i);
       FSDataOutputStream os = fs.create(path);
       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
-      int timePerRecord = ((i < numStragglers) ? NORMAL_TASK_TIME :
-        STRAGGLER_TASK_TIME) / RECORDS_PER_FILE;
+      int timePerRecord = ((i < numStragglers) ? STRAGGLER_TASK_TIME :
+        NORMAL_TASK_TIME) / RECORDS_PER_FILE;
       for (int j = 0; j < RECORDS_PER_FILE; j++) {
         bw.write(timePerRecord + "\n");
       }
--- src/main/java/com/cloudera/trigspec/TrigSpecMapper.java
+++ src/main/java/com/cloudera/trigspec/TrigSpecMapper.java
@@ -10,7 +10,9 @@ public class TrigSpecMapper extends Mapper<LongWritable, Text, Text, Text> {
   @Override
   public void map(LongWritable key, Text value, Context context)
       throws InterruptedException, IOException {
-    Thread.sleep(Integer.parseInt(value.toString()));
+    int sleepTime = Integer.parseInt(value.toString());
+    System.out.println("About to sleep for " + sleepTime + " seconds");
+    Thread.sleep(sleepTime);
     context.write(new Text("donekey"), new Text("doneval"));
   }
 }
